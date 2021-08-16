bcebc75f27001020f54218def90324fb9a564f29
Jonathon <bloomfield.codes@gmail.com>
2021-08-16 19:45:25 +0000

    Updated code to use template literals instead of concatenation.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("🎈🎈🎈 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🎈🎈🎈 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🥺🥺🥺 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🥺🥺🥺 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
