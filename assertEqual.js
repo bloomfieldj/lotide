a34c4f7008ffcacf8e09d36384991800037485c6
Jonathon <bloomfield.codes@gmail.com>
2021-08-16 19:40:00 +0000

    I assert, you assert, we all assert!

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("🎈🎈🎈 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🥺🥺🥺 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual(1, 1);
+assertEqual("Chris", "Bosh");
