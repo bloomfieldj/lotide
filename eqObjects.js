47379a00767105e4d1c2fc6e28d5f77a4528215d
Jonathon <bloomfield.codes@gmail.com>
2021-08-18 16:06:56 +0000

    Checks if two objects are identical or not, currently supports primitive values and arrays as object properties.

---
+++ eqObjects.js
@@ -0,0 +1,45 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqArrays = function(array1, array2){
+  let resultArray = [];
+  let iterationLength = array1.length;
+  if (array2.length > array1.length){
+    iterationLength = array2.length;
+  }
+  for (let i = 0; i < iterationLength; i++){
+    resultArray.push(assertEqual(array1[i], array2[i]))
+  }
+  
+  return resultArray.indexOf(false) === -1
+  
+}
+
+const eqObjects = function(object1, object2){
+  let resultsArray = []
+  let object1Length = Object.keys(object1).length;
+  let object2Length = Object.keys(object2).length;
+    for( key in object1){
+     
+    if(typeof(object1[key]) === "object"){
+      resultsArray.push(eqArrays(object1[key], object2[key]));
+    } else {
+    resultsArray.push(assertEqual(object1[key], object2[key])); 
+    }
+  }
+  console.log(resultsArray);
+  console.log(object1Length === object2Length ? resultsArray.indexOf(false) === -1: false);
+  
+}
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
