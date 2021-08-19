4f2f4e86c139a108602fdb0992146aff8af50419
Jonathon <bloomfield.codes@gmail.com>
2021-08-19 01:28:36 +0000

    Finds key of a nested object.

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🎈🎈🎈 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🥺🥺🥺 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback){
+  const objectValueArray = Object.values(object);
+  let resultsArray = objectValueArray.map(callback);
+  let keyIndex =  resultsArray.indexOf(true)
+  return Object.keys(object)[keyIndex];   
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "noma")
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri")
