aaa66e47ed434e3976fe56865de27357503529ff
Jonathon <bloomfield.codes@gmail.com>
2021-08-23 15:45:57 +0000

    Updated function for use with other files.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`🎈🎈🎈 Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`🥺🥺🥺 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 };
 
