7f7d8f3f70e43458bf577ce45a2ddc43282cb005
Jonathon <bloomfield.codes@gmail.com>
2021-08-23 16:13:18 +0000

    Returns last element in array.

--- tail.js
+++ tail.js
@@ -2,9 +2,11 @@ const assertEqual = require('./assertEqual');
 
 const tail = function (array) {
   
-let arrayTail = array.slice(1, array.length - 1);
+  let arrayTail = array[array.length - 1];
   return arrayTail;
+
 };
+
 
 module.exports = tail;
 
