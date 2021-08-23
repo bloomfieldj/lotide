e383be0448b1e76295972b80f1a3c0953ae233c4
Jonathon <bloomfield.codes@gmail.com>
2021-08-23 14:44:28 +0000

    Exported module for use in other files.

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🎈🎈🎈 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🥺🥺🥺 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (array) {
   let firstItem = array[0];
   return firstItem;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+ 
